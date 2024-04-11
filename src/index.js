991cbbb022e96ed8fe1ec5f272f961ed5cfa5b8e
Dario Villar <dario_villar2001@hotmail.com>
2024-04-11 00:45:40 -0300

    Agregado de Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { DashboardRoutes } from './routers/DashboardRoutes';
 import './styles/styles.scss';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DashboardRoutes />
+    <Analytics />
   </React.StrictMode>
 );
